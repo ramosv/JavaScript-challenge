6ec0ba367f62e2a011b88aff621b76167a56530c
ramosv <ramosvicente255@gmail.com>
2022-03-30 21:38:03 -0600

    01_day_Done

---
+++ my-30-days/01_Day_Intro/datatypes.js
@@ -0,0 +1,17 @@
+
+
+console.log(typeof 'vicente')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+let value1
+let value2
+let value3 
+let value4
+
+let value5 = 'value5'
+let value6 = 6
+
---
+++ my-30-days/01_Day_Intro/variable.js
@@ -0,0 +1,16 @@
+// declare a String, Boolean, Undefined and Null
+
+let fn = 'Vicente'
+let ln = 'Ramos'
+let firstday = true
+let almost = null
+let nothing = undefined
+let ismarried = false
+let country = 'Chile'
+let age = '24'
+s
+
+let ages =24 , job = true, name = vicente, pet = 2
+let myage = 24
+let yourage = 30
+console.log(myage,yourage)
