117bf68667f97c7e59cab9b1fb81a0d28b7ac837
ramosv <ramosvicente255@gmail.com>
2022-04-02 16:26:14 -0600

    Day 2 excercises

---
+++ my-30-days/02_Day_DataTyes/datatypes.js
@@ -0,0 +1,13 @@
+
+
+//Day 2 excercises
+
+let challenge = '30 days of JavaScript'
+
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase)
+console.log(challenge.toLowerCase)
+console.log(challenge.substring(2,8))
+console.log(challenge)
+console.log(challenge.includes('String'))
