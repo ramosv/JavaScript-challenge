6467e4c8f4ffaaf430d415d9abc732eea562c79a
ramosv <ramosvicente255@gmail.com>
2022-04-04 22:31:55 -0600

    Day 03 work

---
+++ my-30-days/03_Day_Booleans/Booleans.js
@@ -0,0 +1,27 @@
+let firstname = "Vicente"
+let lastname = "Ramos"
+let Country = "USA"
+let City = "Denver"
+let Age = 24
+let isMarried = false
+let Year = 1998
+
+console.log(typeof firstname)
+console.log(typeof lastname)
+console.log(typeof Country)
+console.log(typeof City)
+console.log(typeof Age)
+console.log(typeof isMarried)
+console.log(typeof Year)
+
+console.log(10 == '10')
+console.log(parseInt('9.8') == 10)
+
+console.log(Math.ceil(4.7) == 5)
+console.log(11 == 13)
+console.log(Age == Math.floor(24.9))
+
+
+//user input
+
+var name = window.prompt("Enter name: "); alert("your name is " + name);
