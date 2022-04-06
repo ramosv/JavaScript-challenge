547ea342ce62099b377456a8b8783724f8c1b7ef
ramosv <ramosvicente255@gmail.com>
2022-04-05 22:22:20 -0600

    Day 03 for loop

--- my-30-days/03_Day_Booleans/Booleans.js
+++ my-30-days/03_Day_Booleans/Booleans.js
@@ -31,3 +31,20 @@ let base = window.prompt("Enter Base: ")
 let height  = window.prompt("Enter height: ")
 
 alert("your area is : "+  base * height* 0.5)
+
+let result = 0
+let result1  = 4 > 3  // true
+let result2  = 4 >= 3  //true
+let result3  = 4 < 3  //false
+let result4  = 4 <= 3 //false
+let result5  = 4 == 4 //true
+let result6  = 4 === 4  //true
+let result7  = 4 != 4  //false
+let result8  = 4 != '4'  //false
+let result9  = 4 == '4'  //true
+const results = [result1,result2, result3, result4, result5, result6,result7,result8,result9]
+
+for(let i = 0; i< 9; i++)
+{
+    console.log(results[i])
+}
