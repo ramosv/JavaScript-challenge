2b308d60579fb9849d8e4fd09c1ea96404afab54
ramosv <56368263+ramosv@users.noreply.github.com>
2022-05-13 13:35:37 -0600

    Arrays

--- my-30-days/05_Day_Arrays/arrays.js
+++ my-30-days/05_Day_Arrays/arrays.js
@@ -1,1 +1,15 @@
 //arrays 
+
+
+const test = ['string0', 'string1' , 'string2', 'string3', 'string4','string5' 'string6']
+
+let length =  test.length
+let first =  test[0]
+
+let find =  test.length / 2
+let middle = test[find]
+let lastindex = test.length -1
+let last = test[lastindex]
+
+const names = ['John', 23, 'Mateo', 89, 'Vicente', 43 ,'Greg', 12]
+const companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle ','Amazon']
